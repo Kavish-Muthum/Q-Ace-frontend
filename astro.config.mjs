b1e1d0567cc6750ae8ba7d4acc377ca8043af834
Kavish Muthum <kavish.muthum@gmail.com>
2024-12-03 13:54:13 -0600

    fix: change ssr type

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,5 +13,6 @@ export default defineConfig({
         'process.env.PERPLEXITY_API_KEY': JSON.stringify(process.env.PERPLEXITY_API_KEY),
         'process.env.OPENAI_API_KEY': JSON.stringify(process.env.OPENAI_API_KEY)
     }
-  } 
+  } ,
+  output: 'server'
 });

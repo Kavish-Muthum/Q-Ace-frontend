dcc3f1bfac1e4303442ad0808b691d7873ce8905
Kavish Muthum <kavish.muthum@gmail.com>
2024-12-03 14:19:40 -0600

    fix: remove adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,15 +8,5 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
-  output: 'server',
-  adapter: node({
-      mode: 'standalone'
-  }),
-  vite: {
-    define: {
-        'process.env.PERPLEXITY_API_KEY': JSON.stringify(process.env.PERPLEXITY_API_KEY),
-        'process.env.OPENAI_API_KEY': JSON.stringify(process.env.OPENAI_API_KEY)
-    }
-  }
+  integrations: [tailwind(), mdx(), sitemap(), icon()]
 });

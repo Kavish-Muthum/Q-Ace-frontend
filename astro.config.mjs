af12d410ae49e6e0ea20486163096ba0f21e9b08
Kavish Muthum <kavish.muthum@gmail.com>
2024-12-03 14:18:28 -0600

    fix: change astrojs version

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,14 +9,14 @@ import node from '@astrojs/node';
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  output: 'server',
+  adapter: node({
+      mode: 'standalone'
+  }),
   vite: {
     define: {
         'process.env.PERPLEXITY_API_KEY': JSON.stringify(process.env.PERPLEXITY_API_KEY),
         'process.env.OPENAI_API_KEY': JSON.stringify(process.env.OPENAI_API_KEY)
     }
-  },
-  output: 'server',
-  adapter: node({
-      mode: 'standalone'
-  })
+  }
 });

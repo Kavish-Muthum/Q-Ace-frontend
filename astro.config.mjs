f26179de046d465c6620f4f5c994c47ecf8e878b
Kavish Muthum <kavish.muthum@gmail.com>
2024-12-03 13:44:37 -0600

    fix: remove python from vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import python from 'vite-plugin-python';
 
 // https://astro.build/config
 export default defineConfig({
